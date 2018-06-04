687c5e0d8a6d0c9f13dcff3001a78270a51df5dc
patryckx <eng,patrickmonteiro@gmail.com>
2018-06-04 12:15:33 -0300

    Criação das skills e Experiência profissional :chart_with_upwards_trend:

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
     <!-- <div class="columns is-multiline is-centered columns-head">
         <div class="column is-9 ">
           <div class="columns column-background">
-            
             <div class="column is-6 is-12-mobile has-text-centered-mobile has-text-left-desktop">
               <figure class="image is-128x128 figure-head" >
                 <img src="http://patrickmonteiro.com.br/img/patrickmonteiro3.png" class="img-circle-head">
@@ -20,7 +19,6 @@
                 <p>Teste</p>
             </div>
           </div>
-        
         </div>
     </div> -->
     <div>
@@ -60,7 +58,6 @@
                     <img src="http://patrickmonteiro.com.br/img/patrickmonteiro3.png" class="img-circle-head">
                   </figure>
                 </div>
-                
               </div>
             </div>
 
@@ -71,36 +68,98 @@
       <section>
         <div class="container">
           <div class="tile is-ancestor">
-            
               <div class="tile is-vertical is-4">
                 <div class="tile">
                   <div class="tile is-parent is-vertical">
                     <article class="tile is-child notification is-primary">
-                      <!-- <p class="title">Vertical...</p> -->
-                      <p class="is-size-5"> <icon name="map-pin" scale="1.1" style="width: 1em"></icon>  Belém-PA</p> 
-                      <p class="is-size-5"> <icon name="envelope" scale="1.1" style="width: 1em"></icon>  Belém-PA</p> 
-                      <p class="is-size-5"> <icon name="envelope" scale="1.1" style="width: 1em"></icon> <a href="http:www.patrickmonteiro.com.br">patrickmonteiro.com.br</a></p>         
+                    <div class="padding-bottom-2">
+                      <p class="is-size-6 is-block "> <icon name="map-pin" scale="1.1" class="margin-left-2 "></icon> Belém-PA</p>
+                      <p class="is-size-6 is-block"> <icon name="envelope" scale="1.1" class="margin-left-2"></icon> eng.patrickmonteiro@gmail.com</p>
+                      <p class="is-size-6 is-block"> <icon name="globe" scale="1.1" class="margin-left-2"></icon> <a class="link-site" href="http:www.patrickmonteiro.com.br">patrickmonteiro.com.br</a></p>
+                    </div>
+                    <!-- </article>
+                    <article class="tile is-child notification is-primary"> -->
+                    <div>
+                      <p class="title">Skills</p>
+                      <p class="is-size-6 padding-bottom-1">
+                        Vue.js
+                        <progress class="progress is-danger" value="85" max="100">90%</progress> 
+                      </p>
+                      <p class="is-size-6 padding-bottom-1">
+                        Codeigniter
+                        <progress class="progress is-danger" value="80" max="100">90%</progress> 
+                      </p>
+                      <p class="is-size-6 padding-bottom-1">
+                        Bootstrap
+                        <progress class="progress is-danger" value="83" max="100">90%</progress> 
+                      </p>
+                       <p class="is-size-6 padding-bottom-1">
+                        Bulma
+                        <progress class="progress is-danger" value="55" max="100">90%</progress> 
+                      </p>
+                      <p class="is-size-6 padding-bottom-1">
+                        PHP
+                        <progress class="progress is-danger" value="60" max="100">90%</progress> 
+                      </p>
+                       <p class="is-size-6 padding-bottom-1">
+                        JavaScript
+                        <progress class="progress is-danger" value="70" max="100">90%</progress> 
+                      </p>
+                      <p class="is-size-6 padding-bottom-1">
+                        ASP Classic
+                        <progress class="progress is-danger" value="60" max="100">60%</progress> 
+                      </p>
+                       <p class="is-size-6 padding-bottom-1">
+                        HTML5
+                        <progress class="progress is-danger" value="90" max="100">90%</progress> 
+                      </p>
+                       <p class="is-size-6 padding-bottom-1">
+                        CSS3
+                        <progress class="progress is-danger" value="60" max="100">90%</progress> 
+                      </p>
+                    </div>
                     </article>
-                    <!-- <article class="tile is-child notification is-warning">
-                      <p class="title">...tiles</p>
-                      <p class="subtitle">Bottom tile</p>
-                    </article> -->
                   </div>
                 </div>
               </div>
-              
               <div class="tile is-vertical is-8">
                 <div class="tile">
                   <div class="tile is-parent is-vertical">
                     <article class="tile is-child notification is-primary is-bold ">
-                      <p class="title">Sobre Mim</p>
-                      <p class="is-size-5">
-                        Sou um Abaetetubense morando em Belém há mais ou menos 10 anos. Trabalho como Analista de Sistemas na W3AS, 
-                        sendo o responsável por otimizar, melhorar e inovar a arquitetura Front-end dos novos produtos, e ajudar na migração de 
+                      <p class="title">Sobre</p>
+                      <p class="is-size-6">
+                      Sou um Abaetetubense morando em Belém há mais ou menos 10 anos. Trabalho como Analista de Sistemas na W3AS,
+                        sendo o responsável por otimizar, melhorar e inovar a arquitetura Front-end dos novos produtos, e ajudar na migração de
                         produtos legados. Possuo certificação internacional em UX, e busco disseminar a cultura UX pela empresa.</p>
-                      <p  class="is-size-5">
+                      <p  class="is-size-6">
                         Nas horas vagas escrevo artigos para o blog <a href="http://viladosilicio.com.br">Vila do Silício</a>. Gosto de vôlei, livros, séries, rock e games.
                       </p>
+                    </article>
+                    <article class="tile is-child notification is-primary is-bold content">
+                      <p class="title">Experiências</p>
+                      <ul>
+                        <li>
+                           <p class="padding-bottom-2">
+                            <span class="is-size-4 is-block"> Analista de Sistemas </span>
+                            <span class="is-size-6">
+                              Sou um Abaetetubense morando em Belém há mais ou menos 10 anos. Trabalho como Analista de Sistemas na W3AS,
+                              sendo o responsável por otimizar, melhorar e inovar a arquitetura Front-end dos novos produtos, e ajudar na migração de
+                              produtos legados. Possuo certificação internacional em UX, e busco disseminar a cultura UX pela empresa.
+                            </span>
+                          </p>
+                        </li>
+                    
+                        <li>
+                          <p class="padding-bottom-2">
+                            <span class="is-size-4 is-block"> Desenvolvedor Web </span>
+                            <span class="is-size-6">
+                              Sou um Abaetetubense morando em Belém há mais ou menos 10 anos. Trabalho como Analista de Sistemas na W3AS,
+                              sendo o responsável por otimizar, melhorar e inovar a arquitetura Front-end dos novos produtos, e ajudar na migração de
+                              produtos legados. Possuo certificação internacional em UX, e busco disseminar a cultura UX pela empresa.
+                            </span>
+                          </p>
+                        </li>
+                      </ul>
                     </article>
                     <!-- <article class="tile is-child notification is-warning">
                       <p class="title">...tiles</p>
@@ -109,7 +168,6 @@
                   </div>
                 </div>
               </div>
-              
             </div>
 
         </div>
@@ -149,6 +207,27 @@ section {
 .teste {
    border: 1px solid black;
 }
+
+.margin-left-2{
+  width: 2em;
+}
+
+.padding-bottom-1{
+  padding-bottom: 0.7em;
+}
+
+.padding-bottom-2{
+  padding-bottom: 2em;
+}
+
+.link-site {
+  text-decoration: none !important;
+}
+
+.link-site:hover {
+  color: hsl(348, 100%, 61%) !important;
+}
+
 
 /* .column{
   margin: 0.2em;
